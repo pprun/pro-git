435facbaa1caf15b4bdc4d5e5133867b34aa604f
pprun <pprun@pprun-hadventure>
2013-05-30 11:35:03 +0800

    init check in

---
+++ src/test/pprun.java
@@ -0,0 +1,23 @@
+package test;
+
+/*
+ *  Copyright (c) 2013 北京海达网畅信息技术开发有限公司（www.hadventure.com）. All rights reserved.
+ * 
+ *  HADVENTURE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * 
+ */
+
+/**
+ *
+ * @author <a href="mailto:zhigang.pi@hadventure.com">pprun</a>
+ */
+public class pprun {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
